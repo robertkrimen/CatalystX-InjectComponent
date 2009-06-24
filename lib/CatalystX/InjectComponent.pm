6f2545e9b712f64af29c69eceb4fb980d7056e50
robertkrimen <robertkrimen@gmail.com>
2009-06-24 00:49:55 -0700

    Added parent dependency

--- lib/CatalystX/InjectComponent.pm
+++ lib/CatalystX/InjectComponent.pm
@@ -9,11 +9,11 @@ CatalystX::InjectComponent - Inject components into your Catalyst application
 
 =head1 VERSION
 
-Version 0.020
+Version 0.021
 
 =cut
 
-our $VERSION = '0.020';
+our $VERSION = '0.021';
 
 =head1 SYNOPSIS
 
