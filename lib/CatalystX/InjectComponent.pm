2149ec13ce7d8cd6ffd6e14bc3d67e28bf11b692
robertkrimen <robertkrimen@gmail.com>
2009-10-07 22:26:57 -0700

    Version bump to 0.022 with dependency fix from bobtfish

--- lib/CatalystX/InjectComponent.pm
+++ lib/CatalystX/InjectComponent.pm
@@ -9,11 +9,11 @@ CatalystX::InjectComponent - Inject components into your Catalyst application
 
 =head1 VERSION
 
-Version 0.021
+Version 0.022
 
 =cut
 
-our $VERSION = '0.021';
+our $VERSION = '0.022';
 
 =head1 SYNOPSIS
 
